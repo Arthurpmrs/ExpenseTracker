c5acc65786fb35415b8e9d75f8a7e8bfeecc149f
arthurpmrs <arthurpmrs@gmail.com>
2021-07-17 22:00:46 -0300

    Console App structure. Easier

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExpenseTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account("Banco do Brasil", 150);
+            account.MakeDeposit(120);
+            account.MakeDeposit(-30);
+            account.MakeDeposit(1500);
+            account.MakeDeposit(-433);
+            account.History();
+        }
+    }
+}
