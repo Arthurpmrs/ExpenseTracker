77fd34ee07b613591299ddc5aa830b4545015823
Arthur <arthurpmrs@gmail.com>
2021-09-18 10:37:46 -0300

    Added Show all entities command.

---
+++ src/Application/ApplicationCommands/ShowCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.ApplicationCommands
+{
+    public class ShowCommand
+    {
+        private Dictionary<string, Account> Accounts;
+
+        public ShowCommand(Dictionary<string, Account> accounts)
+        {
+            this.Accounts = accounts;
+        }
+
+        public void ShowAllEntries()
+        {
+            foreach (KeyValuePair<string, Account> acc in this.Accounts)
+            {
+                Console.WriteLine($"Account: {acc.Value.Name}");
+                Console.WriteLine($"------------------------------------------");
+                Console.WriteLine(" ");
+
+                foreach (KeyValuePair<string, Transfer> tr in acc.Value.Transfers)
+                {
+                    Console.WriteLine($"     Transfer: {tr.Value.Name}");
+
+                    foreach (Transaction t in tr.Value.Transactions)
+                    {
+                        Console.WriteLine($"         Value: {t.Value} | Date: {t.Date} | Note: {t.Note}");
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine($"Account Balance: R$ {acc.Value.GetBalance()}");
+                //double balance = 0;
+                //foreach (Transaction tt in acc.Value.Transactions)
+                //{
+                //    Console.WriteLine(tt.Value);
+                //    balance += tt.Value;
+                //}
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -17,136 +17,52 @@ namespace ConsoleApp
             ApplicationStarterCommand starterCommand = new ApplicationStarterCommand(dbname);
             Dictionary<string, Account> accounts = starterCommand.Load();
 
-            foreach (KeyValuePair<string, Account> item in accounts)
-            {
-                Console.WriteLine($"Conta: {item.Key}");
-                foreach (Transaction t in item.Value.Transactions)
-                {
-                    Console.WriteLine($"{t.Value} | {t.Note} | {t.Date}");
-                }
-                Console.WriteLine(" ");
-            }
+            ShowCommand showCommand = new ShowCommand(accounts);
+            showCommand.ShowAllEntries();
 
 
 
 
+
+            //AddSomeTransactions(dbname, accounts);
         }
-        public static void LoadObjects()
+
+        public static void AddSomeTransactions(string dbname, Dictionary<string, Account> accounts)
         {
-            string dbname = "Arthurpmrs2";
+            // Handlers for db access
             DBHandler accountHandler = DBHandlerFactory.Create(HandlerType.Account, dbname);
             DBHandler transferHandler = DBHandlerFactory.Create(HandlerType.Transfer, dbname);
             DBHandler transactionHandler = DBHandlerFactory.Create(HandlerType.Transaction, dbname);
 
+            Transfer pixBB = accounts["ContaCorrenteBB"].Transfers["PixBB"];
+            Transfer transferenceBB = accounts["ContaCorrenteBB"].Transfers["Conta Corrente"];
 
-            LoadAccountsCommand command1 = new LoadAccountsCommand(accountHandler);
-            Dictionary<string, Account> accounts = command1.Load();
-            foreach (KeyValuePair<string, Account> item in accounts)
-            {
-                Account acc = item.Value;
-                Console.WriteLine($"Acc. ID: {acc.ID} | Acc. Name: {acc.Name} | Acc. Bank: {acc.Bank}");
-                LoadTransfersCommand command2 = new LoadTransfersCommand(transferHandler, acc);
-                Dictionary<string, Transfer> transfers = command2.Load();
-                Console.WriteLine(" ");
-                Console.WriteLine($"    Transfers:");
-                foreach (KeyValuePair<string, Transfer> transferItem in transfers)
-                {
-                    Transfer t = transferItem.Value;
-                    Console.WriteLine($"    ID: {t.ID} | Nome: {t.Name} | Type: {t.Type}");
-                    LoadTransactionsCommand command3 = new LoadTransactionsCommand(transactionHandler, acc, t);
-                    Console.WriteLine($"        Transactions:");
-                    try
-                    {
-                        List<Transaction> transactions = command3.LoadTransferTransactions();
-                        foreach (Transaction tr in transactions)
-                        {
-                            Console.WriteLine($"        Value: R${tr.Value} | Transfer: {tr.TransferID}");
-                        }
-                        Console.WriteLine(" ");
-
-                    }
-                    catch (EmptyStorageException e)
-                    {
-                        Console.WriteLine($"        Transfer >{t.Name}< has no Transaction entries.");
-                    }
-                }
-                Console.WriteLine(" ");
-            }
-        }
-        public static void AddSomeEntries()
-        {
-            // Adding new entries simulation
-            string dbname = "Arthurpmrs2";
-            DBHandler accountHandler = DBHandlerFactory.Create(HandlerType.Account, dbname);
-            DBHandler transferHandler = DBHandlerFactory.Create(HandlerType.Transfer, dbname);
-            DBHandler transactionHandler = DBHandlerFactory.Create(HandlerType.Transaction, dbname);
-
-            Dictionary<string, Account> accounts = new Dictionary<string, Account>();
-            CreateAccountCommand accountCommand = new CreateAccountCommand(accountHandler);
-            accounts.Add("ContaCorrenteBB", accountCommand.Create("ContaCorrenteBB", "BB"));
-            accountCommand.GetStoredTransactions(transactionHandler, accounts["ContaCorrenteBB"]);
-
-            accounts.Add("Poupança", accountCommand.Create("Poupança", "CEF"));
-            accountCommand.GetStoredTransactions(transactionHandler, accounts["Poupança"]);
-
-            accounts.Add("ContaCorrenteNB", accountCommand.Create("ContaCorrenteNB", "NuBank"));
-            accountCommand.GetStoredTransactions(transactionHandler, accounts["ContaCorrenteNB"]);
-
-            CreateTransferCommand transferCommand = new CreateTransferCommand(transferHandler, accounts["ContaCorrenteBB"]);
-            transferCommand.Create("Pix", "PixBB", "arthurpmrs@gmail.com");
-            transferCommand.Create("Transference", "Conta Corrente", "475454");
-
-            CreateTransferCommand transferCommand2 = new CreateTransferCommand(transferHandler, accounts["Poupança"]);
-            transferCommand2.Create("Transference", "Poupança", "475454");
-
-            CreateTransferCommand transferCommand3 = new CreateTransferCommand(transferHandler, accounts["ContaCorrenteNB"]);
-            transferCommand3.Create("Pix", "PixNB", "06348234428");
-
-            Console.WriteLine("Transactions");
-            foreach (KeyValuePair<string, Account> entry in accounts)
-            {
-                foreach (Transaction t in entry.Value.Transactions)
-                {
-                    Console.WriteLine($"{t.Value} | {t.Note}");
-                }
-            }
-            //CreateTransactionCommand transactionCommand1 = new CreateTransactionCommand(transactionHandler,
-            //                                                                            accounts["ContaCorrenteBB"],
-            //                                                                            accounts["ContaCorrenteBB"].Transfers["PixBB"]);
-            //transactionCommand1.Create(-13.90, "Eletronic", "Cabo USB para carregador de celular", "09/09/2021");
-            //transactionCommand1.Create(-259.00, "Eletronic", "Chromecast 3", "03/09/2021");
-            //transactionCommand1.Create(-233.99, "Eletronic", "Mouse Bluetooth", "20/08/2021");
-            //transactionCommand1.Create(-229.90, "Eletronic", "Teclado Bluetooth", "18/08/2021");
-
-            //CreateTransactionCommand transactionCommand2 = new CreateTransactionCommand(
-            //    transactionHandler,
-            //    accounts["ContaCorrenteBB"],
-            //    accounts["ContaCorrenteBB"].Transfers["Conta Corrente"]
-            //    );
-            //transactionCommand2.Create(1500, "Bolsa", "Bolsa Julho", "05/07/2021");
-            //transactionCommand2.Create(1500, "Bolsa", "Bolsa Agosto", "05/08/2021");
-            //transactionCommand2.Create(1500, "Bolsa", "Bolsa Setembro", "05/09/2021");
-            //transactionCommand2.Create(-990, "Eletronic", "Fones Bluetooth", "25/08/2021");
-            //transactionCommand2.Create(250, "Eletronic", "Pagamento fone Bluetooth mãe", "25/08/2021");
-
-            //CreateTransactionCommand transactionCommand3 = new CreateTransactionCommand(
-            //    transactionHandler,
-            //    accounts["Poupança"],
-            //    accounts["Poupança"].Transfers["Transference"]
-            //    );
-            //transactionCommand3.Create(3000, "Initial", "Budget Inicial", "01/01/2021");
-            //transactionCommand3.Create(-1500, "Rent", "Aluguel campinas mais multas", "15/05/2021");
-
-            //CreateTransactionCommand transactionCommand4 = new CreateTransactionCommand(
-            //    transactionHandler,
-            //    accounts["ContaCorrenteNB"],
-            //    accounts["ContaCorrenteNB"].Transfers["PixNB"]
-            //    );
-
-            //transactionCommand4.Create(7000, "Initial", "Transferência inicial para pagar notebook", "09/09/2021");
-            //transactionCommand4.Create(-64591010, "Eletronic", "Notebook", "09/09/2021");
+            Transfer TransferecneCEF = accounts["Poupança"].Transfers["Poupança"];
+            Transfer pixNB = accounts["ContaCorrenteNB"].Transfers["PixNB"];
 
 
+
+            CreateTransactionCommand conn_pixBB = new CreateTransactionCommand(transactionHandler, accounts["ContaCorrenteBB"], pixBB);
+            conn_pixBB.Create(50, "Avó", "Passar fotos do celular", "20/08/2020");
+            conn_pixBB.Create(-236, "Eletronics", "Amazon Fire Stick");
+            conn_pixBB.Create(-250, "Eletronics", "Teclado Bluetooth", "25/07/2021");
+            conn_pixBB.Create(-212, "Eletronics", "Mouse Bluetooth", "27/07/2021");
+            conn_pixBB.Create(1500, "Mestrado", "Bolsa Mês Agosto", "05/08/2021");
+            conn_pixBB.Create(1500, "Mestrado", "Bolsa Mês Setembro", "05/09/2021");
+
+            CreateTransactionCommand conn_transferenceBB = new CreateTransactionCommand(transactionHandler, accounts["ContaCorrenteBB"], transferenceBB);
+            conn_transferenceBB.Create(-2200, "Aluguel", "Pagamento taxas de quebra de contrato aluguel", "19/05/2021");
+            conn_transferenceBB.Create(-700, "Eletronics", "Fones de ouvido");
+
+            CreateTransactionCommand conn_TransferecneCEF = new CreateTransactionCommand(transactionHandler, accounts["Poupança"], TransferecneCEF);
+            conn_TransferecneCEF.Create(1900, "Lend", "Pagamento dinheiro emprestado mãe.");
+            conn_TransferecneCEF.Create(-20, "Seguro", "Seguro mês 7", "15/07/2021");
+            conn_TransferecneCEF.Create(-215, "Eletronics", "ChormeCast", "26/08/2021");
+
+            CreateTransactionCommand conn_pixNB = new CreateTransactionCommand(transactionHandler, accounts["ContaCorrenteNB"], pixNB);
+            conn_pixNB.Create(500, "Inicial", "Saldo Inicial");
+            conn_pixNB.Create(-78, "Eletronics", "Multímetro Digital");
+            conn_pixNB.Create(-38, "Software", "Chave windows 10 amazon");
         }
     }
 }
